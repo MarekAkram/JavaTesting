d0d6d10f31e49ef1322c3d24ff1aa1f6efe613d5
Marek <sincereakram@gmail.com>
2023-04-27 15:08:07 +0300

    Sepa

--- js/number.js
+++ js/number.js
@@ -6,12 +6,3 @@ console.log(b);
 
 const c = 3;
 console.log(c);
-
-const a1 = 'Kokia';
-console.log(a1);
-
-const a2 = 'puiki';
-console.log(a2);
-
-const a3 = 'diena';
-console.log(a3);
---
+++ js/string1.js
@@ -0,0 +1,8 @@
+const a1 = 'Kokia';
+console.log(a1);
+
+const a2 = 'puiki';
+console.log(a2);
+
+const a3 = 'diena';
+console.log(a3);
