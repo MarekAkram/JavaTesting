6c704a792d860c8516c0fcb39405f4f7f3348739
Marek <sincereakram@gmail.com>
2023-04-27 15:05:15 +0300

    testuojam

---
+++ js/number.js
@@ -0,0 +1,17 @@
+const a = 8;
+console.log(a);
+
+const b = 9;
+console.log(b);
+
+const c = 3;
+console.log(c);
+
+const a1 = 'Kokia';
+console.log(a1);
+
+const a2 = 'puiki';
+console.log(a2);
+
+const a3 = 'diena';
+console.log(a3);
