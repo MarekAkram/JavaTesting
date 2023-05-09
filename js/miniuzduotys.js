da4cc12ad0c7fa1dbe2eab78eb21989f4369cbee
Marek <sincereakram@gmail.com>
2023-05-09 15:08:58 +0300

    funkcija

--- js/miniuzduotys.js
+++ js/miniuzduotys.js
@@ -1,55 +1,55 @@
  console.clear();
 /*1 Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
 */
-const amzius = 39;
-console.log(amzius);
+const nr1 = 3;
+console.log(nr1);
 
-const metai = 1984;
-console.log(metai);
+const nr2 = 198;
+console.log(nr2);
 
-const diena = 30;
-console.log(diena);
+const nr3 = 30;
+console.log(nr3);
 
 /*Susumuoti visus skaičiaus tipo kintamuosius
 */
 
-const suma = amzius + metai +diena
+const suma = nr1 + nr2 + nr3
 console.log(suma)
 
 /*2*/
-const spalva = 'geltona';
-console.log(spalva)
-const spalva1 = 'zalia';
-console.log(spalva1)
-const  spalva2 = 'raudona';
-console.log(spalva2)
+const pvd = 'masina';
+console.log(pvd)
+const pvd1 = 'zalias';
+console.log(pvd1)
+const  pvd2 = 'mersas';
+console.log(pvd2)
 
 /*Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
 */
 
-const text = spalva + ' ' + spalva1 + ' ' + spalva2;
+const text = pvd + ' ' + pvd1 + ' ' + pvd2;
 console.log(text);
 
 /*Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas*/
-const text1 =  spalva2 + ', ' + spalva1 + ', ' + spalva;
+const text1 =  pvd2 + ', ' + pvd1+ ', ' + pvd;
 console.log(text1);
 
 
 
 /*Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
 */
-const v = [1, 2, 3, 4, 5];
-console.log(v);
-const d = [5, 8, 4, 9, 3];
-console.log(d);
-const t = [4, 8, 5, 1, 7];
-console.log(t);
+const k = [1, 2, 3, 4, 5];
+console.log(k);
+const l = [5, 8, 4, 9, 3];
+console.log(l);
+const m = [4, 8, 5, 1, 7];
+console.log(m);
 
-console.log('v','d','t');
+
+console.log('k','l','m');
 
 /*Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką 1-2+3-4+5*/
-const tipai = v - d + t
-console.log(tipai)
+
 
 
 
@@ -57,44 +57,115 @@ console.log(tipai)
 console.log('-----');
 /*Lyginant, jei rezultatas tenkina palyginimo sąlygą, tai į console išvesti žodį “Pomidoras”, o jei sąlyga nėra tenkinama, išvesti sakinį “Bandykite kitą kartą.”*/
 
-const a = 5;
-const b = 7;
-const c = 2;
+let a = 5;
+let b = 7;
+
 
 if (a > b) {
     console.log('Pomidoras');
-} else {
-    console.log('Bandykite dar karta');
+
+} else  {
+    console.log('Bandykite dar kartą')
 }
+
 if (a < b) {
     console.log('Pomidoras');
-} else {
-    console.log('Bandykite dar karta');
+
+} else  {
+    console.log('Bandykite dar kartą')
 }
 
 if (a === b) {
     console.log('Pomidoras');
-} else {
-    console.log('Bandykite dar karta');
+
+} else  {
+    console.log('Bandykite dar kartą')
 }
 
 if (a !== b) {
     console.log('Pomidoras');
-} else {
-    console.log('Bandykite dar karta');
+
+} else  {
+    console.log('Bandykite dar kartą')
 }
 
-if (a <= b) {
+let Text1 = "Tekstas";
+
+let Text2 = "Tekstas1";
+
+console.log(Text1.length);
+console.log(Text2.length);
+
+ let text1Length = Text1.length;
+ let text2Length = Text2.length;
+
+if (text1Length > text2Length) {
     console.log('Pomidoras');
 } else {
-    console.log('Bandykite dar karta');
+    console.log('Bandykite dar kartą');
 }
 
-if (a >= b) {
+if (text1Length < text2Length) {
     console.log('Pomidoras');
 } else {
-    console.log('Bandykite dar karta');
+    console.log('Bandykite dar kartą');
 }
 
+
+if (text1Length === text2Length) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar kartą');
+}
+
+
+if (text1Length !== text2Length) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar kartą');
+}
+
+
+if (text1Length >= text2Length) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar kartą');
+}
+
+if (text1Length <= text2Length) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar kartą');
+}
+
+const orai = ['lietus', 'sauleta', 'giedra', 'puga' ];
+const kur = ['kaime', 'mieste', 'vienkiemi' ];
+ 
+const orai1 = orai.length;
+const kur1 = kur.length;
+
+console.log(orai1, kur1);
+
+const orai2 = orai[0].length;
+const orai3 = orai[1].length;
+const orai4 = orai[2].length;
+const orai5 = orai[3].length;
+
+
+console.log(orai2, orai3, orai4, orai5);
+
+if (kur1 > orai1) {
+    console.log('pomidoras');
+}
+else {
+    console.log('Bandykite dar kartą');
+}
+
+/* Funkcijos */ 
+
+function tusciaFunkcija() {
+    return false;
+}
+console.log(tusciaFunkcija());
 
 
